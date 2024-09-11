ff70fe66248b5403ba4f607659e8db89e1d64dbf
shelalaa20 <165301542+shelalaa20@users.noreply.github.com>
2024-09-11 21:35:24 +0700

    Update 2-encapsulation.php

--- jobsheet1/2-encapsulation.php
+++ jobsheet1/2-encapsulation.php
@@ -6,12 +6,6 @@ class Mahasiswa {
     private $nim;
     private $jurusan;
     
-    // Constructor untuk menginisialisasi objek Mahasiswa
-    public function __construct($nama, $nim, $jurusan) {
-        $this->nama = $nama;      // Inisialisasi nama
-        $this->nim = $nim;        // Inisialisasi nim
-        $this->jurusan = $jurusan; // Inisialisasi jurusan
-    }
 
     // Method untuk mendapatkan nilai nama
     public function getNama() {
@@ -45,20 +39,14 @@ class Mahasiswa {
 }
 
 // Membuat objek Mahasiswa baru dengan data awal
-$mahasiswa1 = new Mahasiswa("Shela Jaya Andini", "230302044", "Komputer dan Bisnis");
+$mahasiswa1 = new Mahasiswa();
 
-// Menampilkan data awal mahasiswa
-echo "Nama: " . $mahasiswa1->getNama() . "<br>";
-echo "NIM: " . $mahasiswa1->getNim() . "<br>";
-echo "Jurusan: " . $mahasiswa1->getJurusan() . "<br><br>";
-
-// Mengubah nilai properti menggunakan setter
+// Mengatur nilai properti menggunakan setter
 $mahasiswa1->setNama("Chitoo");
 $mahasiswa1->setNim("230102078");
 $mahasiswa1->setJurusan("Kedokteran Hewan");
 
-// Menampilkan data setelah diubah
-echo "<b>Data setelah diubah:<br></b>";
+// Menampilkan data 
 echo "Nama: " . $mahasiswa1->getNama() . "<br>";
 echo "NIM: " . $mahasiswa1->getNim() . "<br>";
 echo "Jurusan: " . $mahasiswa1->getJurusan() . "<br>";
